e462d92174179cf42afda63314acaf7fb1df4b63
WoodenBrick1 <brickwooden@gmail.com>
2025-01-09 19:09:11 +0100

    Made the Hashmap relocate the elements when it increases

--- test.js
+++ test.js
@@ -14,8 +14,11 @@ test.set('ice cream', 'white')
 test.set('jacket', 'blue')
 test.set('kite', 'pink')
 test.set('lion', 'golden')
-test.set('Noooo', 'Nopee')
-test.set('Hell nah', 'well maybe')
-test.set('yup', 'yeah')
-test.set('pass', 'word')
+
+
+console.log(test.getCapacity());
+
+test.set('moon', 'silver');
+console.log(test.getCapacity());
+console.log(test.length());
 console.log(test.entries());
